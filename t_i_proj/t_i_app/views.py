7273e2612d944ebda4f94017f81070c393128d5f
Kevin Yancy <kevin@code-crew.org>
2019-02-25 13:30:47 -0600

    check

--- t_i_proj/t_i_app/views.py
+++ t_i_proj/t_i_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+cars = ['Honda', 'Toyota', 'GM', 'Nissan']
+
 # Create your views here.
+def base(request):
+    print('Ace of the base')
+    return render(request, 't_i_app/base.html',)
